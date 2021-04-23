c93cb78d5ea69ba187bb22fd48a58bc89956f30a
jessicasewell <sewell.jessica95@gmail.com>
2021-04-23 19:14:46 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = (arr) => {
+
+  arr.map(sentence => {
+    const words = sentence.split(" ")
+      const newSentence = words.map(word => word[0].toUpperCase() 
+      + word.slice(1))
+    return newSentence.join(" ")
+  
+})
+  }
